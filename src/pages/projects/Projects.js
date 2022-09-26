4ea267c481a88d8469c0845e5c8b435435680342
Colin <colinjoe9@gmail.com>
2022-09-26 19:00:09 -0400

    yea

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -11,6 +11,31 @@ function Projects(props) {
         })
     })
 
+    const projects = [
+        {
+            title: "valorant-rpc",
+            github: "colinhartigan/valorant-rpc",
+            description: "a discord rich presence implementation for VALORANT featuring a full set of art assets and descriptive presence states",
+            languages: ["python"],
+            technologies: ["webserver, discord API, valorant API"],
+        },
+        {
+            title: "valorant-skin-cli",
+            github: "colinhartigan/valorant-skin-cli",
+            description: "(predecessor to valorant-inventory-manager) a command line interface for managing a VALORANT inventory with additional features not in the game client",
+            languages: ["python"],
+            technologies: ["webserver, cli interface, valorant API"],
+        },
+        {
+            title: "ggsheet",
+            github: "colinhartigan/ggsheet",
+            description: "a program to generate post-match summary images, similar to the ones used in VALORANT esports competitions",
+            languages: ["python"],
+            technologies: ["image processing, valorant API"],
+        },
+        
+    ]
+
     return (
         <div className="h-fit w-2xl max-w-screen-2xl mx-auto flex flex-col items-start justify-start">
             <div className="w-[95%] h-auto grow mx-auto flex flex-col items-start justify-center pt-20 pb-8">
@@ -25,9 +50,12 @@ function Projects(props) {
 
                 <div className="w-[80%] h-[2px] m-10 bg-black self-center"/>
 
-                <div className="w-full h-auto grid grid-cols-3 gap-12 justify-center items-center">
+                <div className="w-full h-min grid grid-cols-3 gap-12 justify-center items-start">
 
-                    <Project />
+                    {projects.map((project, index) => {
+                        const data = project;
+                        return <Project data={data}/>
+                    })}
 
                 </div>
 
