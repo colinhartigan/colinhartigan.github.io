00f0108697a25715350bfd8afbbe217c8f9cde0a
Colin <colinjoe9@gmail.com>
2023-03-28 17:02:35 -0400

    yippie

--- src/pages/home/newHome.js
+++ src/pages/home/newHome.js
@@ -14,15 +14,15 @@ function Home(props) {
 
     return (
         <>
-            <div className="w-[80%] h-screen max-w-screen-2xl mx-auto">
+            <div className="w-[80%] h-screen max-w-screen-7xl mx-auto">
                 <div className="h-full w-auto mx-auto flex flex-col items-center justify-center pt-12">
 
-                    <div className="relative w-full h-[90%] border-black border-2 flex flex-row items-center justify-center">
+                    <div className="relative w-full h-[90%] max-h-[800px] border-black border-2 flex flex-row items-center justify-center">
 
                         <div className="absolute w-auto left-0 bottom-0 h-auto flex flex-col items-center justify-center z-10 bg-transparent">
 
                             <div className="w-full h-auto border-y-2 border-black flex flex-row items-center justify-start bg-white">
-                                <p className="text-5xl p-3 font-medium h-full w-auto text-center">
+                                <p className="text-5xl p-3 font-medium h-full w-auto text-left">
                                     colin hartigan
                                 </p>
                                 <div className="w-auto h-full flex grow bg-black border-black" />
@@ -46,12 +46,12 @@ function Home(props) {
 
                             <div className="w-full h-auto border-b-0 border-black flex flex-row items-center justify-start bg-white">
                                 <a href="https://github.com/colinhartigan" target="_blank" rel="noreferrer">
-                                    <p className="text-lg p-2 h-full border-r-2 border-black w-auto text-center">
+                                    <p className="text-lg p-2 h-full border-r-2 border-black w-auto text-center underline">
                                         github
                                     </p>
                                 </a>
                                 <a href="https://www.linkedin.com/in/colin-hartigan/" target="_blank" rel="noreferrer" >
-                                    <p className="text-lg p-2 h-full border-r-2 border-black w-auto text-center">
+                                    <p className="text-lg p-2 h-full border-r-2 border-black w-auto text-center underline">
                                         linkedin
                                     </p>
                                 </a>
@@ -61,7 +61,7 @@ function Home(props) {
                         </div>
 
 
-                        <img src={portrait} alt="portrait" className="relative w-full h-full object-cover grayscale z-0" />
+                        <img src={portrait} alt="portrait" className="relative w-full h-full object-cover grayscale z-0 border-black border-2" />
 
                     </div>
 
--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -5,7 +5,7 @@ function NotFound(props) {
         <div className="h-screen w-screen flex flex-col items-center justify-end">
             <embed className="flex-grow max-w-[1000px] h-auto pt-16" title="resume" src={resume} width="100%" height="auto" type="application/pdf" />
             <div className="h-16 pt-4">
-                <p className="text-xl">Last updated: 01/11/2023</p>
+                <p className="text-xl">last updated: 03/28/2023</p>
             </div>
         </div>
     )
