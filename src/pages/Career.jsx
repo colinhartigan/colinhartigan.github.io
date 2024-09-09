cc3e1cb090e0ca10d27bdba7214f2ad049b38090
colin <42125428+colinhartigan@users.noreply.github.com>
2024-09-08 23:05:37 -0400

    Update Career.jsx

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,11 +1,9 @@
-import { PiArrowSquareRightThin } from "react-icons/pi";
-
 export default function Career({}) {
     const jobs = ["APL", "APL", "IntelliGenesis LLC", "Lenovo", "???"];
 
     return (
-        <div className="flex h-full w-full flex-col items-center justify-start">
-            <div className="flex w-full flex-row items-center justify-center gap-4 text-2xl">
+        <div className="flex h-full flex-col items-center justify-start gap-4">
+            {/* <div className="flex w-full flex-row items-center justify-center gap-4 text-2xl">
                 {jobs.map((org, i) => {
                     return (
                         <>
@@ -14,6 +12,26 @@ export default function Career({}) {
                         </>
                     );
                 })}
+            </div> */}
+
+            <div className="flex w-5/6 flex-col items-center justify-start gap-4">
+                {/* <div className=""></div> */}
+                <div className="flex h-auto w-full flex-col items-start justify-start gap-4">
+                    <div className="flex w-full flex-row items-center justify-start gap-4">
+                        <div className="flex h-auto w-full flex-row items-center justify-between">
+                            <h1 className="text-4xl italic">Lenovo</h1>
+                            <h2 className="text-xl">June 2024 - August 2024</h2>
+                        </div>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-start gap-4">
+                        <div className="flex h-full w-[40px] flex-col items-center justify-center">
+                            <div className="border- h-full w-[2px] rounded-lg bg-black"></div>
+                        </div>
+                        <div className="flex h-auto w-full flex-row items-center justify-between">
+                            blah blah blah company stuff
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
